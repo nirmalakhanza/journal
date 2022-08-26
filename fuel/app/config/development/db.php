36c603bb1a93a6fbee7c88b652cda91345dc9447
cakmoel <alanmoehammad@gmail.com>
2022-08-27 05:01:52 +0700

    Initial commit

---
+++ fuel/app/config/development/db.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Fuel is a fast, lightweight, community driven PHP 5.4+ framework.
+ *
+ * @package    Fuel
+ * @version    1.8.2
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2019 Fuel Development Team
+ * @link       https://fuelphp.com
+ */
+
+/**
+ * -----------------------------------------------------------------------------
+ *  Database settings for development environment
+ * -----------------------------------------------------------------------------
+ *
+ *  These settings get merged with the global settings.
+ *
+ */
+
+return array(
+	'default' => array(
+		'connection' => array(
+			'dsn'      => 'mysql:host=localhost;dbname=fuel_dev',
+			'username' => 'root',
+			'password' => '5oeL4ka1987!@#',
+		),
+		'profiling' => true,
+	),
+);
